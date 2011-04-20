9db74483f6f0b2cdd44bf2780f3dbcef258d9bb4
Chris Davis <chrismd@gmail.com>
2011-04-20 14:58:31 -0500

    moving ceres-maintenance into carbon

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ for root, dirs, files in os.walk('webapp/content'):
 
 setup(
   name='graphite-web',
-  version='0.9.8',
+  version='1.1.0',
   url='https://launchpad.net/graphite',
   author='Chris Davis',
   author_email='chrismd@gmail.com',
